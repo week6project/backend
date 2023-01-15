50babf1300d536497a48c76a9be0c750c84d8624
thursdaycurry <thursdaycurry@gmail.com>
2023-01-15 19:42:58 +0900

    [FEATURE] add createPost input validator

--- repositories/posts.repository.js
+++ repositories/posts.repository.js
@@ -3,15 +3,9 @@ const { Posts, sequelize } = require("../models");
 class PostRepository {
   findAllPost = async () => {
     const result = await Posts.findAll({});
-    console.log(`controller hello`);
     return result;
   };
 
-  /**
-   * postId으로 특정 게시글 1개를 찾는 함수
-   * @param { String } postId
-   * @returns 특정 게시글 1개에 대한 객체 반환
-   */
   findPostById = async (postId) => {
     const result = await Posts.findOne({ where: { postId } });
     return result;
@@ -21,11 +15,11 @@ class PostRepository {
     const { userNo, image, inputAnswer, inputHint, difficult } = postInputArgs;
 
     const result = await Posts.create({
-      userNo: userNo,
-      imageSrc: image,
-      inputAnswer: inputAnswer,
-      inputHint: inputHint,
-      difficult: difficult,
+      userNo,
+      image,
+      inputAnswer,
+      inputHint,
+      difficult,
     });
     return result;
   };
