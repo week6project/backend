51217869c065d6a1c733b02fa62364e3916597b5
Daniel Lee <114923190+jhoon8903@users.noreply.github.com>
2023-01-16 20:20:20 +0900

    유저 양식 백엔드 검증 추가

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,21 +1,35 @@
 const UsersServices = require('../services/users.service');
+const { signupForm, loginForm } = require('../helpers/checkForm.helper');
 
 /** 클라이언트에서 넘어온 user관련 API는 해당 method로 분기처리되어 진행 */
 class UsersController {
   usersServices = new UsersServices();
+
   signupProcess = async (req, res) => {
+    const resultSchema = await signupForm(req.body, res);
+    if (resultSchema.status) {
+      return res.status(resultSchema.status).json(resultSchema.message);
+    }
     const { userId, nickname, email, password } = await req.body;
     const signup = await this.usersServices.signupProcess(userId, nickname, email, password);
     return res.status(signup.status).json(signup.data);
   };
 
   loginProcess = async (req, res) => {
+    const resultSchema = await loginForm(req.body, res);
+    if (resultSchema.status) {
+      return res.status(resultSchema.status).json(resultSchema.message);
+    }
     const { userId, password } = await req.body;
     const login = await this.usersServices.loginProcess(userId, password, res);
     return res.status(login.status).json(login.data);
   };
 
   resetProcess = async (req, res) => {
+    const resultSchema = await signupForm(req.body, res);
+    if (resultSchema.status) {
+      return res.status(resultSchema.status).json(resultSchema.message);
+    }
     const { userId, email, password } = await req.body;
     const reset = await this.usersServices.resetProcess(userId, email, password);
     return res.status(reset.status).json(reset.data);
---
+++ helpers/checkForm.helper.js
@@ -0,0 +1,139 @@
+const Joi = require('joi');
+const error = new Error();
+error.status = {};
+exports.signupForm = (req, res) => {
+  const checkuserId = /^[a-zA-Z0-9]{3,10}$/;
+  const checkPassword = /^[a-zA-Z0-9]{4,30}$/;
+  const checkEmail = /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+  const userSchema = Joi.object({
+    userId: Joi.string().pattern(checkuserId).required(),
+    email: Joi.string().pattern(checkEmail).required(),
+    password: Joi.string().pattern(checkPassword).required(),
+    confirm: Joi.string(),
+  });
+  try {
+    const resultSchema = userSchema.validate(req);
+    if (resultSchema.error) {
+      if (resultSchema.error.message.includes('userId')) {
+        error.status = 412;
+        error.message = { errorMessage: '아이디 양식을 확인해주세요' };
+        throw error;
+      }
+      if (resultSchema.error.message.includes('email')) {
+        error.status = 412;
+        error.message = { errorMessage: '이메일 양식을 확인해주세요' };
+        throw error;
+      }
+      if (resultSchema.error.message.includes('password')) {
+        error.status = 412;
+        error.message = { errorMessage: '비밀번호 양식을 확인해주세요' };
+        throw error;
+      }
+    } else {
+      if (resultSchema.value.password !== resultSchema.value.confirm) {
+        error.status = 412;
+        error.message = { errorMessage: '비밀번호가 일치하지 않습니다.' };
+        throw error;
+      }
+
+      return resultSchema;
+    }
+  } catch (error) {
+    if (error.status !== 412) {
+      error.status = 400;
+      error.message = { errorMessage: '예기치못한 오류가 발생하였습니다.' };
+      return error;
+    }
+    return error;
+  }
+};
+
+exports.loginForm = (req, res) => {
+  const checkNickname = /^[a-zA-Z0-9]{3,10}$/;
+  const checkPassword = /^[a-zA-Z0-9]{4,30}$/;
+  const userSchema = Joi.object({
+    userId: Joi.string().pattern(checkuserId).required(),
+    password: Joi.string().pattern(checkPassword).required(),
+  });
+  try {
+    const resultSchema = userSchema.validate(req);
+    if (resultSchema.error) {
+      if (resultSchema.error.message.includes('userId')) {
+        error.status = 412;
+        error.message = { errorMessage: '아이디 또는 비밀번호 양식을 확인해주세요' };
+        throw error;
+      }
+      if (resultSchema.error.message.includes('password')) {
+        error.status = 412;
+        error.message = { errorMessage: '아이디 또는 비밀번호 양식을 확인해주세요' };
+        throw error;
+      }
+    }
+    return resultSchema;
+  } catch (error) {
+    if (error.status !== 412) {
+      error.status = 400;
+      error.message = { errorMessage: '예기치못한 오류가 발생하였습니다.' };
+      return error;
+    }
+    return error;
+  }
+};
+
+exports.postForm = (req, res) => {
+  const RE_TITLE = /^[a-zA-Z0-9\s\S]{1,20}$/;
+  const RE_HTML_ERROR = /<[\s\S]*?>/;
+  const RE_CONTENT = /^[\s\S]{1,50}$/;
+  const postSchema = Joi.object({
+    title: Joi.string()
+      .pattern(RE_TITLE || RE_HTML_ERROR)
+      .required(),
+    content: Joi.string().pattern(RE_CONTENT).required(),
+  });
+  const resultSchema = postSchema.validate(req);
+  try {
+    if (resultSchema.error) {
+      if (resultSchema.error.message.includes('title')) {
+        error.status = 412;
+        error.data = { errorMessage: '게시글 제목 형식을 확인해주세요.' };
+        throw error;
+      }
+      if (resultSchema.error.message.includes('content')) {
+        error.status = 412;
+        error.data = { errorMessage: '게시글 내용 형식을 확인해주세요.' };
+        throw error;
+      }
+    }
+    return resultSchema;
+  } catch (error) {
+    if (error.status !== 412) {
+      error.status = 400;
+      error.data = { errorMessage: '예기치못한 오류가 발생하였습니다.' };
+      return error;
+    }
+    return error;
+  }
+};
+
+exports.commentForm = (req, res) => {
+  const RE_COMMENT = /^[\s\S]{1,100}$/;
+  const commentSchema = Joi.object({
+    comment: Joi.string().pattern(RE_COMMENT).required(),
+  });
+  const resultSchema = commentSchema.validate(req);
+  try {
+    if (resultSchema.error) {
+      error.status = 412;
+      error.data = { errorMessage: '코멘트 내용 형식을 확인해주세요.' };
+      throw error;
+    }
+    return resultSchema;
+  } catch (error) {
+    if (error.status !== 412) {
+      error.status = 400;
+      error.data = { errorMessage: '예기치못한 오류가 발생하였습니다.' };
+      return error;
+    }
+    return error;
+  }
+};
