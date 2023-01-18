bb54d83e147a3c1466b5946c986caaa6239007ac
thursdaycurry <thursdaycurry@gmail.com>
2023-01-18 15:07:14 +0900

    [DOCS] just prettier..

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,5 +1,9 @@
-const UsersServices = require('../services/users.service');
-const { signupForm, loginForm } = require('../helpers/checkForm.helper');
+const UsersServices = require("../services/users.service");
+const {
+  signupForm,
+  loginForm,
+  resetForm,
+} = require("../helpers/checkForm.helper");
 
 /** 클라이언트에서 넘어온 user관련 API는 해당 method로 분기처리되어 진행 */
 class UsersController {
@@ -13,7 +17,12 @@ class UsersController {
       return res.status(resultSchema.status).json(resultSchema.message);
     }
     const { userId, nickname, email, password } = await req.body;
-    const signup = await this.usersServices.signupProcess(userId, nickname, email, password);
+    const signup = await this.usersServices.signupProcess(
+      userId,
+      nickname,
+      email,
+      password
+    );
     return res.status(signup.status).json(signup.data);
   };
 
@@ -28,12 +37,16 @@ class UsersController {
   };
 
   resetProcess = async (req, res) => {
-    const resultSchema = await signupForm(req.body, res);
+    const resultSchema = await resetForm(req.body, res);
     if (resultSchema.status) {
       return res.status(resultSchema.status).json(resultSchema.message);
     }
     const { userId, email, password } = await req.body;
-    const reset = await this.usersServices.resetProcess(userId, email, password);
+    const reset = await this.usersServices.resetProcess(
+      userId,
+      email,
+      password
+    );
     return res.status(reset.status).json(reset.data);
   };
 }
