21d06527b0efe49b04e23727308e38b66c01b0e1
Daniel Lee <114923190+jhoon8903@users.noreply.github.com>
2023-01-20 03:16:34 +0900

    rollback (error)

--- services/posts.service.js
+++ services/posts.service.js
@@ -32,34 +32,9 @@ class PostService {
     const passedPeople = post.Answers.map((value) => {
       return value.User.nickname;
     });
-
-    //     /**
-    //      * passedUserNo정답자를 특정하기 위한 매핑 (nickname 중복자도 거를 수 있음)
-    //      * nickname으로 비교시 중복자 나오는데 이러면 회원가입에서 중복자 막아야함
-    //      * 프론트에서 현재 사용자의 userNo와 passedUserNo 비교해서 인풋박스 막을 예정
-    //      */
-    try {
-      const passedUserNo = await post.Answers.map((value) => {
-        if (passedUserNo) {
-          return value.User.userNo;
-        } else {
-          return '0';
-        }
-      });
-      console.log('패스 유저넘버', '있으면 배열 없으면 0', passedUserNo);
-      //// 여기부터 정답자 비교 로직
-
-      const matchUser = await passedUserNo.includes(userNo);
-      console.log('정답자 확인', matchUser);
-      if (matchUser) {
-        return 'match';
-      }
-    } catch (err) {
-      console.log('패스유저넘버 catch err', err);
-      return 'unMatch';
-    }
-
-    /////////////////////////////////////
+    const passedUserNo = await post.Answers.map((value) => {
+      return value.User.userNo;
+    });
     return {
       id: post.postId,
       postId: post.postId,
@@ -71,8 +46,8 @@ class PostService {
       difficult: post.difficult,
       inputHint: post.inputHint,
       passedPeople: passedPeople,
-      // passedUserNo: passedUserNo,
-      matchUser: matchUser,
+      passedUserNo: passedUserNo,
+      // matchUser: matchUser,
     };
   };
 
